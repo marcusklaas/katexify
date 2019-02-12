3c287dc729b293f05ee14dc66f665f04bccb1c54
Marcus Klaas <mail@marcusklaas.nl>
2019-02-12 15:09:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const katex = require('katex');
+const fs = require('fs').promises;
+const path = require('path');
+
+const walk = async (dir, filelist = []) => {
+    const files = await fs.readdir(dir);
+
+    for (file of files) {
+        const filepath = path.join(dir, file);
+        const stat = await fs.stat(filepath);
+
+        if (stat.isDirectory()) {
+            filelist = await walk(filepath, filelist);
+        } else {
+            filelist.push(filepath);
+        }
+    }
+
+    return filelist;
+}
+
+const katexifyFile = async (filePath) => {
+    if (path.extname(filePath).toLowerCase() == '.html') {
+        const contents = await fs.readFile(filePath, 'utf8');
+        const katexifiedContents = 
+            contents.replace(/\$\$(.*?)\$\$/g, (outer, inner) => katex.renderToString(inner, { displayMode: true }))
+                .replace(/\$(.*?)\$/g, (outer, inner) => katex.renderToString(inner, { displayMode: false }));
+
+        await fs.writeFile(filePath, katexifiedContents);
+        console.log(`Katexified ${filePath}`);
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+const dir = process.argv.length >= 3 ? process.argv[2] : './';
+
+const fileList = walk(dir)
+    .then(fileList => Promise.all(fileList.map(katexifyFile)).then(l => l.reduce((a, b) => a + b, 0)))
+    .then(count => console.log(`Katexified ${count} files without errors!`))
+    .catch(e => console.warn(`Oh no, we got the following error: ${e}`));
---
+++ readme.md
@@ -0,0 +1,10 @@
+# katexify
+
+Recursively walks a filesystem, katexifying all html files in its path. Turns `$...$` spans into inline math, and `$$...$$` into display math.
+
+## Usage
+
+```bash
+node index.js <dir>
+```
+
